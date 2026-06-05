e951ea900dd6bb6689b5f009982f14d11333eb7a
francozeta <perufra@hotmail.com>
2026-06-04 19:48:49 -0500

    fix: validate nested client creation fields

--- src/VianaHub.Global.Gerit.Api/Validators/Business/Client/CreateClientRouteValidator.cs
+++ src/VianaHub.Global.Gerit.Api/Validators/Business/Client/CreateClientRouteValidator.cs
@@ -28,5 +28,80 @@ public class CreateClientRouteValidator : AbstractValidator<CreateClientRequest>
             .MaximumLength(1000)
             .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Note.MaximumLength", 1000))
             .When(x => !string.IsNullOrWhiteSpace(x.Note));
+
+        RuleFor(x => x.Individual!.FirstName)
+            .MaximumLength(100)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.FirstName.MaximumLength", 100))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.FirstName));
+
+        RuleFor(x => x.Individual!.LastName)
+            .MaximumLength(100)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.LastName.MaximumLength", 100))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.LastName));
+
+        RuleFor(x => x.Individual!.PhoneNumber)
+            .MaximumLength(50)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.PhoneNumber.MaximumLength", 50))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.PhoneNumber));
+
+        RuleFor(x => x.Individual!.CellPhoneNumber)
+            .MaximumLength(50)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.CellPhoneNumber.MaximumLength", 50))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.CellPhoneNumber));
+
+        RuleFor(x => x.Individual!.Email)
+            .MaximumLength(100)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.Email.MaximumLength", 100))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.Email));
+
+        RuleFor(x => x.Individual!.Gender)
+            .MaximumLength(20)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.Gender.MaximumLength", 20))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.Gender));
+
+        RuleFor(x => x.Individual!.DocumentType)
+            .MaximumLength(50)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.DocumentType.MaximumLength", 50))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.DocumentType));
+
+        RuleFor(x => x.Individual!.DocumentNumber)
+            .MaximumLength(50)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.DocumentNumber.MaximumLength", 50))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.DocumentNumber));
+
+        RuleFor(x => x.Individual!.Nationality)
+            .MaximumLength(100)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Individual.Nationality.MaximumLength", 100))
+            .When(x => x.Individual != null && !string.IsNullOrWhiteSpace(x.Individual.Nationality));
+
+        RuleFor(x => x.Company!.LegalName)
+            .MaximumLength(200)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Company.LegalName.MaximumLength", 200))
+            .When(x => x.Company != null && !string.IsNullOrWhiteSpace(x.Company.LegalName));
+
+        RuleFor(x => x.Company!.TradeName)
+            .MaximumLength(200)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Company.TradeName.MaximumLength", 200))
+            .When(x => x.Company != null && !string.IsNullOrWhiteSpace(x.Company.TradeName));
+
+        RuleFor(x => x.Company!.Site)
+            .MaximumLength(500)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Company.Site.MaximumLength", 500))
+            .When(x => x.Company != null && !string.IsNullOrWhiteSpace(x.Company.Site));
+
+        RuleFor(x => x.Company!.CompanyRegistration)
+            .MaximumLength(50)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Company.CompanyRegistration.MaximumLength", 50))
+            .When(x => x.Company != null && !string.IsNullOrWhiteSpace(x.Company.CompanyRegistration));
+
+        RuleFor(x => x.Company!.CAE)
+            .MaximumLength(10)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Company.CAE.MaximumLength", 10))
+            .When(x => x.Company != null && !string.IsNullOrWhiteSpace(x.Company.CAE));
+
+        RuleFor(x => x.Company!.LegalRepresentative)
+            .MaximumLength(150)
+            .WithMessage(localization.GetMessage("Api.Validator.Client.Create.Company.LegalRepresentative.MaximumLength", 150))
+            .When(x => x.Company != null && !string.IsNullOrWhiteSpace(x.Company.LegalRepresentative));
     }
 }
