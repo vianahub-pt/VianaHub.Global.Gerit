8365b4c669d10630ab31426cca370f1a2e18e7e8
contato-vianahub-pt <contato@vianahub.pt>
2026-06-14 12:38:39 +0100

    Removendo campo displayName, no response da API quando for devolvido ClientIndividualDetailResponse

--- src/VianaHub.Global.Gerit.Application/Dtos/Response/Business/Client/ClientIndividualDetailResponse.cs
+++ src/VianaHub.Global.Gerit.Application/Dtos/Response/Business/Client/ClientIndividualDetailResponse.cs
@@ -5,10 +5,9 @@ public class ClientIndividualDetailResponse
     public int Id { get; set; }
     public int TenantId { get; set; }
     public int ClientId { get; set; }
-    public string DisplayName { get; set; }
+    public string FullName { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string FullName { get; set; }
     public string PhoneNumber { get; set; }
     public string CellPhoneNumber { get; set; }
     public bool IsWhatsapp { get; set; }
