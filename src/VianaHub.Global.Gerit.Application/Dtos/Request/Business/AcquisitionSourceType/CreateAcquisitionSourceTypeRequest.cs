835950aff02341af780fe2a6cbaf30720b48f5d1
contato-vianahub-pt <contato@vianahub.pt>
2026-07-15 14:56:59 +0100

    feature(domain): add Code property and translations to AcquisitionSourceType — closes #170

--- src/VianaHub.Global.Gerit.Application/Dtos/Request/Business/AcquisitionSourceType/CreateAcquisitionSourceTypeRequest.cs
+++ src/VianaHub.Global.Gerit.Application/Dtos/Request/Business/AcquisitionSourceType/CreateAcquisitionSourceTypeRequest.cs
@@ -2,6 +2,7 @@ namespace VianaHub.Global.Gerit.Application.Dtos.Request.Business.AcquisitionSou
 
 public class CreateAcquisitionSourceTypeRequest
 {
+    public string? Code { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
 }
