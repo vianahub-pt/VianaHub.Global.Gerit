751d8119ffdf789328d2fe82e9ea49fb9c3f13e4
contato-vianahub-pt <contato@vianahub.pt>
2026-07-16 19:22:21 +0100

    migration(gerit): validar/atualizar UserPreferencesEntity — closes #194

--- src/VianaHub.Global.Gerit.Domain/Validators/Identity/UserPreferences/CreateUserPreferencesValidator.cs
+++ src/VianaHub.Global.Gerit.Domain/Validators/Identity/UserPreferences/CreateUserPreferencesValidator.cs
@@ -11,9 +11,11 @@ public class CreateUserPreferencesValidator : AbstractValidator<UserPreferencesE
         RuleFor(x => x.TenantId).GreaterThan(0).WithMessage(localization.GetMessage("Domain.UserPreferences.TenantIdRequired"));
         RuleFor(x => x.UserId).GreaterThan(0).WithMessage(localization.GetMessage("Domain.UserPreferences.UserIdRequired"));
         RuleFor(x => x.Appearance).NotEmpty().WithMessage(localization.GetMessage("Domain.UserPreferences.AppearanceRequired"));
+        RuleFor(x => x.Appearance).Must(x => x == "light" || x == "dark").WithMessage(localization.GetMessage("Domain.UserPreferences.AppearanceInvalid"));
         RuleFor(x => x.Locale).NotEmpty().WithMessage(localization.GetMessage("Domain.UserPreferences.LocaleRequired"));
         RuleFor(x => x.Timezone).NotEmpty().WithMessage(localization.GetMessage("Domain.UserPreferences.TimezoneRequired"));
         RuleFor(x => x.DateFormat).NotEmpty().WithMessage(localization.GetMessage("Domain.UserPreferences.DateFormatRequired"));
         RuleFor(x => x.TimeFormat).NotEmpty().WithMessage(localization.GetMessage("Domain.UserPreferences.TimeFormatRequired"));
+        RuleFor(x => x.TimeFormat).Must(x => x == "24h" || x == "12h").WithMessage(localization.GetMessage("Domain.UserPreferences.TimeFormatInvalid"));
     }
 }
