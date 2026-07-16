738046f5c74abd77f46294b9aef5d7ca4e762490
contato-vianahub-pt <contato@vianahub.pt>
2026-07-16 19:48:49 +0100

    migration(gerit): remover entidades de Status antigas — closes #196

--- src/VianaHub.Global.Gerit.Application/Dtos/Request/Business/StatusType/UpdateStatusTypeRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace VianaHub.Global.Gerit.Application.Dtos.Request.Business.StatusType;
-
-public class UpdateStatusTypeRequest
-{
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-}
