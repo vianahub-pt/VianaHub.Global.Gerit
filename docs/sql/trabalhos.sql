534da384c083ff7eae6ea43ef44fd540d4ec3b30
contato-vianahub-pt <contato@vianahub.pt>
2026-07-19 07:26:36 +0100

    docs

--- docs/sql/trabalhos.sql
+++ docs/sql/trabalhos.sql
@@ -1,48 +1,66 @@
-select * from dbo.AcquisitionSourceTypes;
-select * from dbo.ConsentOriginTypes;
-select * from dbo.AddressTypes;
-select * from dbo.FileTypes;
-select * from dbo.ConsentTypes;
-select * from dbo.StatusTypes;
-select * from dbo.Plans;
-select * from dbo.PlanFileRules;
-select * from dbo.Actions;
-select * from dbo.Resources;			-- R32
-select * from dbo.Roles;				-- R2
-
-
 EXEC sp_set_session_context @key=N'IsSuperAdmin', @value=1;
 
-select * from dbo.Tenants;				-- T1
-select * from dbo.TenantContacts;
+--------------------------------------------
+select * from dbo.AcquisitionSourceTypes;
+select * from dbo.AcquisitionSourceTypeTranslations;
+select * from dbo.AddressTypes;
+select * from dbo.AddressTypeTranslations;
+select * from dbo.DocumentTypes;
+select * from dbo.DocumentTypeTranslations;
+select * from dbo.FileTypes;
+select * from dbo.FileTypeTranslations;
+select * from dbo.PartyTypes;
+select * from dbo.PartyTypeTranslations;
+select * from dbo.StatusDefinitions;
+select * from dbo.StatusDefinitionTranslations;
+select * from dbo.StatusDomains;
+select * from dbo.StatusDomainTranslations;
+select * from dbo.SubscriptionPlans;
+select * from dbo.SubscriptionPlanTranslations;
+
+--------------------------------------------
+select * from dbo.Actions;
+select * from dbo.Resources;
+select * from dbo.Roles;
+
+--------------------------------------------
+select * from dbo.Tenants;
 select * from dbo.TenantAddresses;
+select * from dbo.TenantContactPersons;
+select * from dbo.TenantDocuments;
 select * from dbo.TenantFiscalData;
 select * from dbo.Subscriptions;
 
-select * from dbo.Users;				-- U1
-select * from dbo.UserPreferences;		--
-select * from dbo.UserRoles;			-- T1, U1, R2 
+--------------------------------------------
+select * from dbo.Users;
+select * from dbo.UserPreferences;
+select * from dbo.UserRoles;
 
-select * from dbo.RolePermissions;		-- T1, R2, R32, A1
-select * from dbo.RolePermissions 
-where TenantId = 1
-  and RoleId = 2
-  and ResourceId = 32
-select * from dbo.JwtKeys;
-select * from dbo.JobDefinitions;
-------------------------------
-
+--------------------------------------------
 select * from dbo.Clients;
-select * from dbo.ClientIndividuals;
-select * from dbo.ClientCompanies;
-select * from dbo.ClientContacts;
 select * from dbo.ClientAddresses;
+select * from dbo.ClientContactPersons;
+select * from dbo.ClientDocuments;
 select * from dbo.ClientFiscalData;
-select * from dbo.ClientConsents;
-select * from dbo.ClientHierarchy;
+
 ------------------------------
-select * from dbo.Teams;			--
-select * from dbo.Functions;				--
+select * from dbo.Employees;
+select * from dbo.EmployeeAddresses;
+select * from dbo.EmployeeContactPersons;
+select * from dbo.EmployeeFiscalData;
+select * from dbo.EmployeeTeam;
+------------------------------
+
+select * from dbo.Visits;
+select * from dbo.VisitAddresses;
+select * from dbo.VisitAttachments;
+select * from dbo.VisitContactPersons;
+select * from dbo.VisitTeam;
+select * from dbo.VisitTeamEmployee;
+select * from dbo.VisitTeamEquipment;
+select * from dbo.VisitTeamFunctions;
+select * from dbo.VisitTeamVehicle;
+
 
 select * from dbo.EquipmentTypes;			--
 select * from dbo.Equipments;				--
@@ -50,10 +68,13 @@ select * from dbo.Equipments;				--
 select * from dbo.Vehicles;					--
 
 select * from dbo.FileTypes;
-select * from dbo.PlanFileRules;
-select * from dbo.AttachmentCategories;
 
-FOR JSON AUTO;
+
+select * from dbo.RolePermissions;
+select * from dbo.JwtKeys;
+select * from dbo.JobDefinitions;
+
+--FOR JSON AUTO;
 
 /*
 
